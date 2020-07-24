66b1aa9cd0be793f92198979f03bb89fb5f461bf
Journeymean <larikansver@gmail.com>
2020-07-24 15:55:26 +0300

    loader

--- Gallery/script.js
+++ Gallery/script.js
@@ -49,7 +49,7 @@ function Gallery(arg) {
     let zoomButtonsNode = zoomAreaNode.appendChild(element);
 
     element = document.createElement('div');
-    element.classList.add('loader');
+    element.classList.add('loader','hide');
     element.innerHTML= '<svg width="38" height="38" viewBox="0 0 38 38" stroke="#fff"><g fill="none" fill-rule="evenodd"><g transform="translate(1 1)" stroke-width="2"><circle stroke-opacity=".5" cx="18" cy="18" r="18"/><path d="M36 18c0-9.94-8.06-18-18-18"><animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="1s" repeatCount="indefinite"/></path></g></g></svg>';
     zoomAreaNode.appendChild(element);
    
