c7443b7deb817eada42ab3fd6c7ddb1a5c0a1d93
Journeymean <larikansver@gmail.com>
2020-07-24 15:51:26 +0300

    Update script.js

--- Gallery/script.js
+++ Gallery/script.js
@@ -48,6 +48,11 @@ function Gallery(arg) {
     element.classList.add('buttons');
     let zoomButtonsNode = zoomAreaNode.appendChild(element);
 
+    element = document.createElement('div');
+    element.classList.add('loader');
+    element.innerHTML= '<svg width="38" height="38" viewBox="0 0 38 38" stroke="#fff"><g fill="none" fill-rule="evenodd"><g transform="translate(1 1)" stroke-width="2"><circle stroke-opacity=".5" cx="18" cy="18" r="18"/><path d="M36 18c0-9.94-8.06-18-18-18"><animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="1s" repeatCount="indefinite"/></path></g></g></svg>';
+    zoomAreaNode.appendChild(element);
+   
     if (arg.zoom) {
       element = document.createElement('span');
       element.innerHTML = '<svg  width="512.000000pt" height="512.000000pt" viewBox="0 0 100 100"><path stroke="rgba(0, 0, 0, 0.5)" stroke-width="6" fill="none" stroke-linejoin="round" stroke-linecap="round" d="M 50 20 v 60 " /><path stroke="rgba(0, 0, 0, 0.5)" stroke-width="6" fill="none" stroke-linejoin="round" stroke-linecap="round" d="M 20 50  h 60 " /></svg>';
